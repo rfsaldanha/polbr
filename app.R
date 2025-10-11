12f8414ecd1c47753921aeb4db0be9401f5730a7
Raphael Saldanha <rfsaldanha@gmail.com>
2025-10-11 19:38:26 -0400

    Remove CO map initial state

--- app.R
+++ app.R
@@ -2252,7 +2252,7 @@ server <- function(input, output, session) {
     }
   )
 
-  # Map CO initial state
+  # Map CO
   output$map_co <- renderLeaflet({
     req(input$municipality)
     req(input$forecast)
@@ -2328,57 +2328,6 @@ server <- function(input, output, session) {
     # Update map
     leafletProxy("map_co", session) |>
       addMarkers(lng = coord[1], lat = coord[2], layerId = "mun_marker")
-  })
-
-  # Update raster and date text on map
-  observeEvent(input$forecast, {
-    # Palette
-    mm <- minmax(rst_co)
-
-    # Remove old layers
-    leafletProxy("map_co", session) |>
-      removeImage(layerId = "raster") |>
-      removeVelocity(group = "vento") |>
-      removeControl(layerId = "legend") |>
-      removeControl(layerId = "title")
-
-    # Depth (forecast)
-    depth <- (input$forecast + 1 + 2) / 3
-
-    # Update map
-    leafletProxy("map_co", session) |>
-      addRasterImage(
-        x = rst_co[[depth]],
-        opacity = .7,
-        colors = pal_co,
-        layerId = "raster",
-        project = FALSE,
-        group = "raster"
-      ) |>
-      addVelocity(
-        content = wind_files[depth],
-        group = "vento",
-        layerId = "vento",
-        options = wind_opts
-      ) |>
-      addLegend(
-        pal = pal_co,
-        values = c(min(t(mm)[, 1]), max(t(mm)[, 2])),
-        layerId = "legend",
-        title = paste0("CO (PPM)")
-      ) |>
-      # Layers control
-      addLayersControl(
-        baseGroups = c(
-          "Open Street Maps",
-          "Imagem de satélite"
-        ),
-        overlayGroups = c("raster", "vento"),
-        options = layersControlOptions(
-          collapsed = TRUE,
-          position = "bottomleft"
-        )
-      )
   })
 
   # Graph co
